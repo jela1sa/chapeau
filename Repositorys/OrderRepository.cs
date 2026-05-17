8b2a204707f2c9309a6c5ae664d9a07b953f1a84
RobbeBlaauw <730924@student.inholland.nl>
2026-05-17 11:09:16 +0200

    Update OrderRepository.cs

--- Repositorys/OrderRepository.cs
+++ Repositorys/OrderRepository.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 using Chapeau.Models;
 
 namespace Chapeau.Repositorys
@@ -15,6 +17,7 @@ namespace Chapeau.Repositorys
         public Order GetOrderByTable(int tableId)
         {
             Order order = null;
+            List<OrderItem> orderItems = new();
 
             using (SqlConnection conn = new SqlConnection(_connectionString))
             {
@@ -51,51 +54,52 @@ namespace Chapeau.Repositorys
                     AND b.status = 'Open'
                 ";
 
-                SqlCommand cmd = new SqlCommand(query, conn);
-                cmd.Parameters.AddWithValue("@tableId", tableId);
-
-                SqlDataReader reader = cmd.ExecuteReader();
-
-                List<OrderItem> orderItems = new List<OrderItem>();
-
-                while (reader.Read())
+                using (SqlCommand cmd = new SqlCommand(query, conn))
                 {
-                    if (order == null)
+                    cmd.Parameters.AddWithValue("@tableId", tableId);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
                     {
-                        order = new Order
+                        while (reader.Read())
                         {
-                            OrderId = Convert.ToInt32(reader["bestelling_id"]),
-                            TableId = Convert.ToInt32(reader["tafel_id"]),
-                            StaffId = Convert.ToInt32(reader["bediening_id"]),
-                            DateTime = Convert.ToDateTime(reader["datum_tijd"]),
-                            Status = reader["status"].ToString(),
-                            TimeSpecified = Convert.ToDateTime(reader["tijdstip_opgegeven"]),
-                            OrderItems = orderItems
-                        };
+                            if (order == null)
+                            {
+                                order = new Order
+                                {
+                                    OrderId = Convert.ToInt32(reader["bestelling_id"]),
+                                    TafelId = Convert.ToInt32(reader["tafel_id"]),
+                                    BedieningId = Convert.ToInt32(reader["bediening_id"]),
+                                    DatumTijd = Convert.ToDateTime(reader["datum_tijd"]),
+                                    Status = reader["status"].ToString(),
+                                    TijdstipOpgenomen = Convert.ToDateTime(reader["tijdstip_opgegeven"]),
+                                    OrderItems = orderItems
+                                };
+                            }
+
+                            MenuItem menuItem = new MenuItem(
+                                Convert.ToInt32(reader["item_id"]),
+                                reader["naam"].ToString(),
+                                reader["beschrijving"].ToString(),
+                                Convert.ToDecimal(reader["prijs"]),
+                                reader["categorie"].ToString(),
+                                Convert.ToDecimal(reader["btw_tarief"]),
+                                null
+                            );
+
+                            OrderItem orderItem = new OrderItem
+                            { 
+                                OrderItem_id = Convert.ToInt32(reader["order_item_id"]),
+                                Aantal = Convert.ToInt32(reader["aantal"]),
+                                MenuItem = menuItem
+                            };
+
+                            orderItems.Add(orderItem);
+                        }
                     }
-
-                    MenuItem menuItem = new MenuItem
-                    {
-                        item_id = Convert.ToInt32(reader["item_id"]),
-                        naam = reader["naam"].ToString(),
-                        beschrijving = reader["beschrijving"].ToString(),
-                        prijs = Convert.ToDecimal(reader["prijs"]),
-                        categorie = reader["categorie"].ToString(),
-                        btw_tarief = Convert.ToDecimal(reader["btw_tarief"])
-                    };
-
-                    OrderItem orderItem = new OrderItem
-                    {
-                        order_item_id = Convert.ToInt32(reader["order_item_id"]),
-                        aantal = Convert.ToInt32(reader["aantal"]),
-                        MenuItem = menuItem
-                    };
-
-                    orderItems.Add(orderItem);
                 }
             }
 
-            return bestelling;
+            return order;
         }
     }
 }
