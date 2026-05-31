ff7a458380cdc7c476cfa256736a78cb5dd88090
Jela1sa <752187@student.inholland.nl>
2026-05-31 22:32:49 +0200

    alle opdrachten

--- Repositories/IBestellingRepository.cs
+++ Repositories/IBestellingRepository.cs
@@ -6,5 +6,10 @@ namespace Chapeau.Repositories
     {
         List<Bestelling> GetRunningOrders();
         Bestelling GetOrderByTafel(int Tafel_ID);
+
+        void UpdateOrderStatus(int bestellingId, string status);
+        void UpdateItemStatus(int bestellingsRondeId, string status);
+        void UpdateCourseStatus(int bestellingId, string categorie, string status);
+        List<Bestelling> GetFinishedOrders();
     }
 }
